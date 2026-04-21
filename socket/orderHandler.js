46bbec714322eb296575bf6ff701d0f526a3e0c1
Tuku <tukuwebian@gmail.com>
2026-04-21 21:17:00 +0300

    tracking order listener created

--- socket/orderHandler.js
+++ socket/orderHandler.js
@@ -19,16 +19,30 @@ export const orderHandler = (io, socket) => {
       const totals = calculateTotals(data.items);
       const orderId = generateOrderId();
       const order = createOrderDocument(data, orderId, totals);
-      const orderCollection = getCollection("orders");
-      await orderCollection.insertOne(order);
-      socket.join(`order${orderId}`);
+      const ordersCollection = getCollection("orders");
+      await ordersCollection.insertOne(order);
+      socket.join(`order-${orderId}`);
       socket.join("customers");
-      io.to("admin").emit("newOrder", { order });
+      io.to("admins").emit("newOrder", { order });
       callback({ success: true, order });
       console.log(`order created: ${orderId}`);
     } catch (error) {
       console.log(error);
-      callback({success:false,message:'Failed to place order...'})
+      callback({ success: false, message: "Failed to place order..." });
+    }
+  });
+  socket.on("trackOrder", async (data, callback) => {
+    try {
+      const ordersCollection = getCollection("orders");
+      const order = await ordersCollection.findOne({ orderId: data.orderId });
+      if (!order) {
+        return callback({ success: false, message: "Order not found" });
+      }
+      socket.join(`order-${data.orderId}`);
+      callback({ success: true, order });
+    } catch (error) {
+      console.error("Order tracking error", error);
+      callback({ success: false, message: error.message });
     }
   });
 };
--- utils/helper.js
+++ utils/helper.js
@@ -30,8 +30,8 @@ export function validateOrder(data) {
 export function generateOrderId() {
   const now = new Date();
   const year = now.getFullYear();
-  const month = now.getMonth();
-  const date = now.getDate();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const date = String(now.getDate()).padStart(2, "0");
   const orderNumber = Math.floor(Math.random() * 1000)
     .toString()
     .padStart(3, "0");
